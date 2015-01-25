04e4c987dff48dc80b2f820cfef8b56777b0ce1e
Jonathan Ostrander <jonathanost@gmail.com>
2015-01-25 14:20:58 -0500

    started working on db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for, session, g, send_from_directory
+from flask.ext.sqlalchemy import SQLAlchemy
 import requests
 import json
 import logging
@@ -10,6 +11,31 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I Like big beers'
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    untappd_id = db.Column(db.Integer, index=True)
+    username = db.Column(db.String, index=True)
+    user_avatar = db.Column(db.String)
+    checkins = db.relationship('CheckIn', backref = 'author', lazy = 'dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class CheckIn(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    checkin_id = db.Column(db.Integer, index=True)
+    name = db.Column(db.String)
+    brewery = db.Column(db.String)
+    style = db.Column(db.String)
+    abv = db.Column(db.Float)
+    brewer_country = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<CheckIn - %r>' % self.name
 
 @app.before_request
 def load_access_token():
@@ -29,15 +55,29 @@ def index():
 
 @app.route('/beers')
 def beers():
+    # If their access token is not present then return an error
     if not g.access_token:
         error = {
-          "success": false,
+          "success": False,
           "error": {
             "code": 100,
             "message": "No access token is present."
           }
         }
-        return jsonify(error)
+        return json.dumps(error)
+
+    # Get User info, check if they are in DB, add them if not
+    params = {'access_token': g.access_token}
+    UNTAPPD_URL = "https://api.untappd.com/v4/user/info/"
+    r = requests.get(UNTAPPD_URL, params = params)
+
+    untappd_id, username, user_avatar = r.json()[u'response'][u'user'][u'uid'], r.json()[u'response'][u'user'][u'user_name'], r.json()[u'response'][u'user'][u'user_avatar']
+
+    if not User.query.filter(User.untappd_id == untappd_id):
+        # Add user to database
+        user = User(untappd_id=untappd_id, username=username, user_avatar=user_avatar)
+        db.session.add(user)
+
     params = {'access_token': g.access_token, 'limit': 50}
     UNTAPPD_URL = "https://api.untappd.com/v4/user/checkins/"
     beer_list_untappd = []
@@ -102,6 +142,8 @@ def authorize():
         access_token = r.json()[u'response'][u'access_token']
         session['access_token'] = access_token
     return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     app.run()
---
+++ config.py
@@ -0,0 +1,8 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'fundees99'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
